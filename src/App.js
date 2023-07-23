52b70e0d795e8f5cafdb72d6f07daa627e41d09e
Sandip Thapa <sandipthapa386@gmail.com>
2023-07-23 15:56:11 +0545

    feat(layout): basic layout setup with pages and components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
 import React from "react";
 import "./App.css";
+import { Box } from "@mui/material";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import ExerciseDetail from "./components/ExerciseDetail";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 const App = () => {
-    return <div>App</div>;
+    return (
+        <Box
+            width={"400px"}
+            sx={{
+                width: { xl: "1448px" },
+            }}
+            m={"auto"}
+        >
+            <Navbar />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/exercise/:id" element={<ExerciseDetail />} />
+            </Routes>
+            <Footer />
+        </Box>
+    );
 };
 
 export default App;
