cd7ba9f9c9f564a23938db017462346774830d97
Sandip Thapa <sandipthapa386@gmail.com>
2023-07-23 16:22:11 +0545

    feat(Navbar): Navbar basic layout

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,56 @@
 import React from "react";
+import Logo from "../assets/images/Logo.png";
+import { Stack } from "@mui/material";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-    return <div>Navbar</div>;
+    return (
+        <Stack
+            direction={"row"}
+            justifyContent={"none"}
+            sx={{
+                gap: {
+                    sm: "122px",
+                    xs: "40px",
+                },
+                mt: { sm: "32px", xs: "20px" },
+            }}
+            px={"20px"}
+        >
+            <Link to={"/"}>
+                <img
+                    src={Logo}
+                    alt="Logo"
+                    style={{
+                        height: "48px",
+                        width: "48px",
+                        margin: "0 20px",
+                    }}
+                />
+            </Link>
+            <Stack direction={"row"} gap={"40px"} fontSize={"24px"} alignItems={"flex-end"}>
+                <Link
+                    to="/"
+                    style={{
+                        textDecoration: "none",
+                        color: "#3A1212",
+                        borderBottom: "3px solid #FF2625",
+                    }}
+                >
+                    Home
+                </Link>
+                <a
+                    href="#exercises"
+                    style={{
+                        textDecoration: "none",
+                        color: "#3A1212",
+                    }}
+                >
+                    Exercises
+                </a>
+            </Stack>
+        </Stack>
+    );
 };
 
 export default Navbar;
