f2d6ea66db2f1f3f4f19dc38653d31b3792bb02f
Sandip Thapa <sandipthapa386@gmail.com>
2023-07-26 16:11:37 +0545

    feat(Footer): Basic footer implementation

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,15 @@
 import React from "react";
+import Logo from "../assets/images/Logo-1.png";
+import { Box, Stack } from "@mui/material";
 
 const Footer = () => {
-    return <div>Footer</div>;
+    return (
+        <Box mt={"80px"} bgcolor={"#fff3f4"}>
+            <Stack gap={"40px"} alignItems={"center"} px={"40px"} p={"24px 0"}>
+                <img src={Logo} alt="logo" width={"200px"} height={"40px"} />
+            </Stack>
+        </Box>
+    );
 };
 
 export default Footer;
