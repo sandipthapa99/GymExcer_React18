841cf852e55b984a83e0ae7d0367181f082ca5e8
Sandip Thapa <sandipthapa386@gmail.com>
2023-07-26 09:38:06 +0545

    feat(Search): Search results card complete

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -1,5 +1,6 @@
 import { Box, Stack, Typography } from "@mui/material";
 import React from "react";
+import ExerciseCard from "./ExerciseCard";
 
 const Exercises = ({ exercises, setExercises, bodyPart }) => {
     return (
@@ -24,7 +25,7 @@ const Exercises = ({ exercises, setExercises, bodyPart }) => {
                 justifyContent={"center"}
             >
                 {exercises.map((exercise, index) => (
-                    <p>{exercise.name}</p>
+                    <ExerciseCard key={index} exercise={exercise} />
                 ))}
             </Stack>
         </Box>
