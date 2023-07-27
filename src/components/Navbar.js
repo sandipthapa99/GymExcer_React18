59b7f30ba5b713aa4036a47df69775bf4c5a47ff
Sandip Thapa <sandipthapa386@gmail.com>
2023-07-27 11:38:48 +0545

    fix(Navbar): Logo and menu list edit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -28,13 +28,13 @@ const Navbar = () => {
                     }}
                 />
             </Link>
-            <Stack direction={"row"} gap={"40px"} fontSize={"24px"} alignItems={"center"}>
+            <Stack direction={"row"} gap={"40px"} fontSize={"20px"} alignItems={"center"}>
                 <Link
                     to="/"
                     style={{
                         textDecoration: "none",
                         color: "#3A1212",
-                        borderBottom: "3px solid #FF2625",
+                        // borderBottom: "3px solid #FF2625",
                     }}
                 >
                     Home
